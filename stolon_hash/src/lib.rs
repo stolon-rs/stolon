d96cf16f1997b7b048d7051bc3a4500c19df9c87
Anthony Huynh <anthony@pierule.com>
2022-06-18 17:38:01 -0500

    move git root one level up

---
+++ stolon_hash/src/lib.rs
@@ -0,0 +1,66 @@
+use rayon::prelude::*;
+use sha2::Digest;
+
+/// Usage: stolon-hash::crack::<Sha256>(wordlist_bytes, hashed_password)
+
+pub fn crack<'a, D>(wordlist: &'a [u8], hashed: &'a [u8]) -> Option<&'a [u8]>
+where
+    D: Digest,
+{
+    wordlist
+        .par_split(|i| i.is_ascii_whitespace())
+        .into_par_iter()
+        .find_map_first(|w| {
+            let mut hasher = D::new();
+            hasher.update(w);
+            if *hashed == hasher.finalize()[..] {
+                Some(w)
+            } else {
+                None
+            }
+        })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use sha2::{Sha256, Sha512};
+
+    #[test]
+    fn crack_sha_negative_result() {
+        let mut hasher = Sha256::new();
+        hasher.update("frog");
+        let hashed = hasher.finalize();
+        assert_eq!(
+            crack::<Sha256>(&["dog", "cat", "bat"].join("\n").as_bytes(), &hashed),
+            None
+        );
+
+        assert_eq!(
+            crack::<Sha512>(&["dog", "frog", "bat"].join("\n").as_bytes(), &hashed),
+            None
+        )
+    }
+
+    #[test]
+    fn crack_sha_positive_result() {
+        let mut hasher = Sha256::new();
+        hasher.update("frog");
+        let hashed = hasher.finalize();
+        assert_eq!(
+            crack::<Sha256>(&["dog", "frog", "bat"].join("\n").as_bytes(), &hashed),
+            Some("frog".as_bytes())
+        );
+    }
+
+    #[test]
+    fn crack_sha_with_different_digest() {
+        let mut hasher = Sha512::new();
+        hasher.update("frog");
+        let hashed = hasher.finalize();
+        assert_eq!(
+            crack::<Sha512>(&["dog", "frog", "frog"].join("\n").as_bytes(), &hashed),
+            Some("frog".as_bytes())
+        );
+    }
+}
---
+++ stolon_hash/src/main.rs
@@ -0,0 +1,33 @@
+use sha2::{Digest, Sha256};
+use std::{env, fs, path::PathBuf, process::ExitCode, str};
+use stolon_hash::crack;
+
+fn main() -> ExitCode {
+    let args: Vec<String> = env::args().collect();
+
+    if !(args.len() == 2) {
+        println!("\n\nusage: stolon_hash 'path/to/wordlist'\n\n");
+        return ExitCode::FAILURE;
+    }
+
+    let mut filepath = PathBuf::new();
+    filepath.push::<String>(args[1].parse().unwrap());
+
+    let err = format!("unable to open file: {:?}", &filepath);
+    let wordlist = fs::read(&filepath).expect(&err[..]);
+
+    let mut hasher = Sha256::new();
+    hasher.update("password123");
+    let hashed = hasher.finalize();
+
+    if let Some(result) = crack::<Sha256>(&wordlist[..], &hashed[..]) {
+        println!(
+            "\n\ncracked password is: \t{:?}\n\n",
+            str::from_utf8(result).unwrap()
+        );
+    } else {
+        println!("no password was found for the hash: {:?}", hashed);
+    }
+
+    ExitCode::SUCCESS
+}
