4bedb078933c6c8f59da8cdb0e260f386d04a046
Anthony Huynh <anthony@pierule.com>
2022-06-18 09:14:02 -0500

    change to shortlist

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::path::PathBuf;
 
 use sha2::{Digest, Sha256};
-use stolon_hash::crack_sha;
+use stolon_hash::crack;
 
 fn main() {
-    let mut filepath = env::current_dir().expect("unable to get current dir");
-    filepath.push(PathBuf::from("assets"));
-    filepath.push("rockyou.txt");
+    // let mut filepath = env::current_dir().expect("unable to get current dir");
+    let mut filepath = env::current_dir().unwrap();
+    filepath.push("assets");
+    filepath.push("shortlist.txt");
     let file = File::open(&filepath).expect(&format!("unable to open file {:?}", &filepath)[..]);
     let reader = BufReader::new(&file);
     println!("reading the wordlist at: {:?}...", filepath);
@@ -28,7 +28,7 @@ fn main() {
     hasher.update("password123");
     let hashed = hex::encode(hasher.finalize());
 
-    if let Some(result) = crack_sha::<Sha256>(&wordlist, &hashed) {
+    if let Some(result) = crack::<Sha256>(&wordlist, &hashed) {
         println!("cracked password is: \t{:?}", result);
     } else {
         println!("no password was found for the hash: {:?}", hashed);
