c40c8238b45a8ea4ba2a0478e13b0cb12cfc5633
Anthony Huynh <anthony@pierule.com>
2022-06-18 10:42:03 -0500

    change crack to work with array of bytes

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,24 @@
 use rayon::prelude::*;
 use sha2::Digest;
 
-/// Usage: stolon-hash::crack_sha::<Sha1>(wordlist, password_hash)
+/// Usage: stolon-hash::crack::<Sha256>(wordlist_bytes, hashed_password)
 
-pub fn crack<D>(wordlist: &Vec<String>, hashed: &String) -> Option<String>
+pub fn crack<'a, D>(wordlist: &'a [u8], hashed: &'a [u8]) -> Option<&'a [u8]>
 where
     D: Digest,
 {
-    println!("now cracking the hash: {:?}...", hashed);
-    wordlist.into_par_iter().find_map_first(|w| {
-        let mut hasher = D::new();
-        hasher.update(w.trim().as_bytes());
-        if *hashed == hex::encode(hasher.finalize()) {
-            Some(w.to_owned())
-        } else {
-            None
-        }
-    })
+    wordlist
+        .par_split(|i| i.is_ascii_whitespace())
+        .into_par_iter()
+        .find_map_first(|w| {
+            let mut hasher = D::new();
+            hasher.update(w);
+            if *hashed == hasher.finalize()[..] {
+                Some(w)
+            } else {
+                None
+            }
+        })
 }
 
 #[cfg(test)]
@@ -24,50 +26,41 @@ mod tests {
     use super::*;
     use sha2::{Sha256, Sha512};
 
-    fn setup<D>() -> (String, String)
-    where
-        D: Digest,
-    {
-        let password = "frog".to_string();
-        let mut hasher = D::new();
-        hasher.update(password.as_bytes());
-        let hashed: String = hex::encode(hasher.finalize());
-        (password, hashed)
-    }
-
     #[test]
     fn crack_sha_negative_result() {
-        let (_, hashed) = setup::<Sha256>();
+        let mut hasher = Sha256::new();
+        hasher.update("frog");
+        let hashed = hasher.finalize();
         assert_eq!(
-            crack::<Sha256>(
-                &vec!["dog".to_string(), "cat".to_string(), "bat".to_string()],
-                &hashed
-            ),
+            crack::<Sha256>(&["dog", "cat", "bat"].join("\n").as_bytes(), &hashed),
             None
         );
+
+        assert_eq!(
+            crack::<Sha512>(&["dog", "frog", "bat"].join("\n").as_bytes(), &hashed),
+            None
+        )
     }
 
     #[test]
     fn crack_sha_positive_result() {
-        let (password, hashed) = setup::<Sha256>();
+        let mut hasher = Sha256::new();
+        hasher.update("frog");
+        let hashed = hasher.finalize();
         assert_eq!(
-            crack::<Sha256>(
-                &vec!["dog".to_string(), "frog".to_string(), "bat".to_string()],
-                &hashed
-            ),
-            Some(password)
+            crack::<Sha256>(&["dog", "frog", "bat"].join("\n").as_bytes(), &hashed),
+            Some("frog".as_bytes())
         );
     }
 
     #[test]
-    fn crack_sha_generic_param() {
-        let (password, hashed) = setup::<Sha512>();
+    fn crack_sha_with_different_digest() {
+        let mut hasher = Sha512::new();
+        hasher.update("frog");
+        let hashed = hasher.finalize();
         assert_eq!(
-            crack::<Sha512>(
-                &vec!["dog".to_string(), "frog".to_string(), "frog".to_string()],
-                &hashed
-            ),
-            Some(password)
+            crack::<Sha512>(&["dog", "frog", "frog"].join("\n").as_bytes(), &hashed),
+            Some("frog".as_bytes())
         );
     }
 }
